b19681d217cce1e7872b1afcf971a830f063910c
David Šarman <david.sarman@gmail.com>
2016-06-19 17:55:49 +0200

    Added simple test for cancellation of order

--- features/msg_processing.feature
+++ features/msg_processing.feature
@@ -5,7 +5,8 @@ Feature: Process incoming client messages and respond accordingly if needed to
   @logged_in
   Scenario: Message to create new order is received
     When message with order data is received
-    Then client receives the "1" created orders ids
+    Then order is created
+    And client receives the "1" created orders ids
 
 
   @real_server
@@ -16,3 +17,11 @@ Feature: Process incoming client messages and respond accordingly if needed to
     And message with order data is received
     And message with order data is received
     Then client receives the "3" created orders ids
+
+  @real_server
+  @fake_client
+  @logged_in
+  Scenario: Deletion of existing order
+    When order already exists
+    And message to delete order is received
+    Then order is deleted
--- features/steps/msg_processing.py
+++ features/steps/msg_processing.py
@@ -8,6 +8,14 @@ def step_impl(context):
                          'side': 'BUY',
                          'price': 100,
                          'quantity': 100})
+    context.price = 100
+    context.quantity = 100
+
+
+@then("order is created")
+def step_impl(context):
+    order = context.server.matching_engine.asks[context.price][0]
+    assert_that(order, not_none())
 
 
 @then("client receives the \"{num}\" created orders ids")
@@ -22,3 +30,24 @@ def step_impl(context, num):
             expected_i = start_i + i
         assert_that(reply, equal_to({'type': 'orderCreated',
                                      'id': expected_i}))
+
+
+@when("message to delete order is received")
+def step_impl(context):
+    context.client.send({'message': 'cancelOrder',
+                         'orderId': context.order_id})
+
+
+@step("order already exists")
+def step_impl(context):
+    context.execute_steps(u'''
+        when message with order data is received
+    ''')
+
+    reply = context.client.blocking_recv()
+    context.order_id = reply['id']
+
+
+@then("order is deleted")
+def step_impl(context):
+    assert_that(len(context.server.matching_engine.asks.keys()), equal_to(0), "Limit order book size")
